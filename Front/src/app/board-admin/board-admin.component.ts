99bd183547fa5b2635ccb7976f3add76f6665318
nnouisser <56487570+nnouisser@users.noreply.github.com>
2024-05-26 23:44:39 +0100

    Front

---
+++ Front/src/app/board-admin/board-admin.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { UserService } from '../_services/user.service';
+@Component({
+  selector: 'app-board-admin',
+  templateUrl: './board-admin.component.html',
+  styleUrls: ['./board-admin.component.scss']
+})
+export class BoardAdminComponent implements OnInit {
+  content?: string;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getAdminBoard().subscribe({
+      next: data => {
+        this.content = data;
+      },
+      error: err => {
+        this.content = JSON.parse(err.error).message;
+      }
+    });
+  }
+}
